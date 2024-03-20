cc83eb2d5bfcf5fd6d2083f2c2acdb958bb0cb91
RYZENWALE <arya247aviero@gmail.com>
2024-03-21 00:22:49 +0530

    nothing much

--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addTodo, clearError, deleteTodo, setError } from '../../features/todoFeatures/todoSlice'
 import { TodoState } from '../../types/todo.type'
